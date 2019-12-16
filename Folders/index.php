cca8055ee7cacfd27f39ab75cbcb38541789a182
Eperjesi Attila Dávid <eper.atis@gmail.com>
2019-12-16 17:40:32 +0100

    Base website
    Main html and some css

---
+++ Folders/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Az én lapom</title>
+    <link rel="stylesheet" href="http://localhost:8888/website/css/app.css">
+</head>
+<body>
+<header>
+        <div class="container">
+            <a class="logo" href="#">Eperjesi Attila Dávid</a>
+            <nav>
+                <ul>
+                    <li><a href="#">Rólam</a></li>
+                    <li><a href="#">Kapcsolat</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="container">
+        
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; 2019 MySite All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
