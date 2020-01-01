badcad0ce7dfd56ee6e453b579bcd0bf2ec6fbb5
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-01-01 13:21:29 +0100

    Database
    Add database connection.
    On the "Utasok" tab you can upload passengers and send into the database (phpmyadmin)

---
+++ Folders/connect.php
@@ -0,0 +1,38 @@
+<?php
+$uname = filter_input(INPUT_POST, 'uname');
+$ules = filter_input(INPUT_POST, 'ules');
+if (!empty($uname)){
+if (!empty($ules)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "root";
+$dbname = "website";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO utasok (uname, ules)
+values ('$uname','$ules')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Ülés nem lehet üres.";
+die();
+}
+}
+else{
+echo "Név nem lehet üres.";
+die();
+}
+?>
---
+++ Folders/views/utasok.php
@@ -0,0 +1,12 @@
+<?php include_once "_header.php"; ?>
+<div>
+    <h1>Utasok</h1>
+    <h2>Utasok hozzáadása</h2>
+    <form method="post" action="connect.php">
+        Név : <input type="text" name="uname"><br><br>
+        Ülés : <input type="text" name="ules"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
+
+<?php include_once "_footer.php"; ?>
