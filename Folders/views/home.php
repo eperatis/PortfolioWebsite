7ad3b637315a323119a48737bea95e415cf681e9
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-01-14 13:41:08 +0100

    Some css

--- Folders/views/home.php
+++ Folders/views/home.php
@@ -6,5 +6,6 @@
     Utasok felvétele, moódosítása és törlése lehetséges.<br>
     Kérjül jelentkezzen be a jobb felnti Login menüpont használatával!
     </p>
+    <p>Ha nincs fiókja kattintson a lenti regisztráció gombra.</p>
 </div>
 <?php include_once "_footer.php"; ?>
